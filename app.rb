6c9858a317ea0fc10bbd63cd3910cbf9e58c6389
Joe Nelson <cred+github@begriffs.com>
2013-03-19 14:13:09 -0700

    Enable sessions

--- app.rb
+++ app.rb
@@ -1,7 +1,12 @@
 require 'sinatra'
 require 'oauth/consumer'
 require 'dotenv'
+
 Dotenv.load
+configure do
+  enable :sessions
+  set :session_secret, ENV['SESSION_SECRET']
+end
 
 get '/' do
   "Hello World!"
